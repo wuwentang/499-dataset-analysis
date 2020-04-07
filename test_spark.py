565c06de3f63fe1d6a211479ba6bd5fad3eb5ce2
Zhen-Yee <davster2@gmail.com>
2020-04-07 19:19:48 -0400

    test spark

---
+++ test_spark.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+df = spark.read.csv("data/test_data_review_businesses.csv")
+df.show(100, truncate=True)
