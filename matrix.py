02fe8195d67f4f527a4c0537b76750ff9b13921b
RazvanSoos <razvan.soos@gmail.com>
2020-04-07 18:14:47 -0400

    basic correlation for top 2 restaurants

--- matrix.py
+++ matrix.py
@@ -3,3 +3,20 @@ import pandas as pd
 df = pd.read_csv('data/yelp_reviews_businesses.csv', sep=',')
 #creating matrix with every row being a business and every column being an user. The value is the review number
 matrix = df.pivot_table(index='user_id', columns='business_id', values='review_stars')
+
+df1 = df[['business_id','name','stars']]
+df1 = df1.drop_duplicates()
+df1.sort_values(['stars'],ascending=False).head(10)
+
+veghed_user_rating = matrix['VUADGMPLJoWqhHb1G4LIcA']
+NOseafoodsteak = matrix['J9vAdD2dCpFuGsxPIn184w']
+
+similar_to_veghed = matrix.corrwith(veghed_user_rating)
+similar_to_NOseafoodsteak = matrix.corrwith(NOseafoodsteak)
+
+corr_veghed= pd.DataFrame(similar_to_veghed, columns=['Correlation'])
+corr_veghed.dropna(inplace=True)
+corr_nosea = pd.DataFrame(similar_to_NOseafoodsteak,columns=['Correlation'])
+corr_nosea.dropna(inplace=True)
+
+print(corr_nosea)
