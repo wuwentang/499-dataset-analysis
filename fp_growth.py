1c22d29237496cdbabb3ecee46f13c6be50fdb31
Zhen-Yee <davster2@gmail.com>
2020-04-08 12:29:56 -0400

    FPGrowth with test_data

--- fp_growth.py
+++ fp_growth.py
@@ -32,19 +32,18 @@ def remove_duplicate(x):
 rdd = spark.sparkContext.textFile('data/test_data_review_businesses.csv')
 header = rdd.first()
 data_rdd = rdd.filter(lambda row: row != header).map(lambda row: Row(business_id=row.split(",")[0], user_id=row.split(",")[5]))
+data_rdd = data_rdd.map(lambda row: row).distinct()
 data_df = spark.createDataFrame(data_rdd)
 data_df = data_df[["user_id", "business_id"]]
 data_df = data_df.groupby('user_id').agg(F.collect_list('business_id'))
-
 data_df.show(20, truncate=False)
 
-# user_id_dff.show(20, truncate=False)
 # # Python API docs
-# fpGrowth = FPGrowth(itemsCol="business_id", minSupport=0.5, minConfidence=0.6)
+fpGrowth = FPGrowth(itemsCol="collect_list(business_id)", minSupport=0.5, minConfidence=0.6)
 
 # # model = spark.sparkContext.parallelize(fpGrowth.fit(user_id_df), numSlices=1000)
-# model = fpGrowth.fit(user_id_df)
+model = fpGrowth.fit(data_df)
 
 # # Display frequent itemsets
-# model.freqItemsets.show(20, truncate=False)
+model.freqItemsets.show(20, truncate=False)
 
