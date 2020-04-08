9e1105df3baa5ee3e58439bac992e87475c754e0
Zhen-Yee <davster2@gmail.com>
2020-04-08 12:34:16 -0400

    comments FP-Growth

--- fp_growth.py
+++ fp_growth.py
@@ -12,31 +12,20 @@ spark = SparkSession \
     .config("spark.some.config.option", "some-value") \
     .getOrCreate()
 
-# remove duplicate restaurants
-def remove_duplicate(x):
-    x.drop_duplicates(keep=False, inplace=True)
-    return x.tolist()
-
-# df = pd.read_csv('data/test_data_review_businesses.csv')
-
-# user_business_df = df[['user_id', 'business_id']]
-
-# # group user_id with list of business_id
-# user_id_df = user_business_df.groupby('user_id').agg(lambda x: remove_duplicate(x))
-# print(user_id_df)
-
-
-# # convert pandas df to spark df
-# user_id_dff = spark.createDataFrame(user_id_df)
-
 rdd = spark.sparkContext.textFile('data/test_data_review_businesses.csv')
+
+# removing header
 header = rdd.first()
+
+# filter header out and create user_id -> business_id rdd
 data_rdd = rdd.filter(lambda row: row != header).map(lambda row: Row(business_id=row.split(",")[0], user_id=row.split(",")[5]))
+
+# remove duplicate rows
 data_rdd = data_rdd.map(lambda row: row).distinct()
+
+# create DataFrame
 data_df = spark.createDataFrame(data_rdd)
-data_df = data_df[["user_id", "business_id"]]
 data_df = data_df.groupby('user_id').agg(F.collect_list('business_id'))
-data_df.show(20, truncate=False)
 
 # # Python API docs
 fpGrowth = FPGrowth(itemsCol="collect_list(business_id)", minSupport=0.5, minConfidence=0.6)
